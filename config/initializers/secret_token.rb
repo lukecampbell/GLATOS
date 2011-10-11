85e4d744abe2602f7c6833f7f43349a06bbb7caf
Kyle Wilcox <kwilcox@asascience.com>
2011-10-11 17:05:43 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glatos::Application.config.secret_token = '861227b333298a636d1c22308583c2e5e4d7ae9b099abeda385f8a66dd0156dd52160e9c8a761fcd21ec5584aaf40053b17b4d1fd13e4fd6deb42809c6bebda3'
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Glatos::Application.routes.draw do
+
+  # root :to => 'welcome#index'
+
+end
