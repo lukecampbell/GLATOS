e9c56a1b8740da369c835ab5006535af38ba736f
Kyle Wilcox <kwilcox@asascience.com>
2011-11-07 11:22:05 -0500

    Updates to the Report pages based on comments from meeting on 10/31/2011

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,6 @@
 class Report < ActiveRecord::Base
 
-  validates :tag, :description, :method, :name, :phone, :email, :city, :state, :length, :weight, :fishtype, :found, :presence => true
+  validates :tag, :description, :method, :name, :email, :length, :weight, :fishtype, :found, :presence => true
   validates :email, :format => { :with => /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i, :message => "Invalid Email Address" }
 
   set_rgeo_factory_for_column(:location, RGeo::Geographic.spherical_factory(:srid => 4326))
