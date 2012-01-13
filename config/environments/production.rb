1364d500c1476976913f11913590e03bdf7d8a85
Kyle Wilcox <kwilcox@asascience.com>
2012-01-13 12:36:10 -0500

    update production email host.  fixes #1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Glatos::Application.configure do
   config.assets.precompile += %w( ie7.css ie6.css )
   config.assets.precompile += %w( printing.css )
 
-  config.action_mailer.default_url_options = { :host => 'glatos.org' }
+  config.action_mailer.default_url_options = { :host => 'glatos.asascience.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
