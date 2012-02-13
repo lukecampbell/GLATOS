88c24cc5d6b090bd5558b3e8bb8b2e62a6f4742f
Kyle Wilcox <kwilcox@asascience.com>
2012-02-13 13:06:23 -0500

    Carry over some custom exporting rules.  Requires patch to DataTables.
    See:
    http://datatables.net/extras/tabletools/button_options
    http://www.datatables.net/forums/discussion/6114/fix-for-bug-in-tabletools/p1

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,5 +3,6 @@
 //= require jquery_ujs
 //= require jquery.highlight.min
 //= require jquery.dataTables.min
-//= require jquery.dataTables.tableTools.min
+//= require jquery.dataTables.tableTools.zeroClipboard
+//= require jquery.dataTables.tableTools
 //= require site
