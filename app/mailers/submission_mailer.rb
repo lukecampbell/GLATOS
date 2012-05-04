32f0b07c6f2f1874a84973966d18b7dabf51e6a2
Kyle Wilcox <kwilcox@asascience.com>
2012-05-04 15:34:28 -0400

    Typo in various emails

--- app/mailers/submission_mailer.rb
+++ app/mailers/submission_mailer.rb
@@ -4,7 +4,7 @@ class SubmissionMailer < ActionMailer::Base
     @submission = submission
     recips = @submission.user.email
     bccs = User.where("role = 'admin'").map(&:email)
-    mail(:to => recips, :bcc => bccs, :subject => "[GLATOS] Data Submission Recieved")
+    mail(:to => recips, :bcc => bccs, :subject => "[GLATOS] Data Submission Received")
   end
 
 end
