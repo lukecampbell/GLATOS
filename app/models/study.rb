b693c0cfb1bb89f92e50ee96240a367c03d37ec3
Kyle Wilcox <kwilcox@asascience.com>
2012-05-08 17:08:18 -0400

    Fix Study parsing to not cut off on commas

--- app/models/study.rb
+++ app/models/study.rb
@@ -88,12 +88,12 @@ class Study < ActiveRecord::Base
         lines = f.readlines
         ENV['WEB_ADMIN_PASSWORD'] ||= "default"
         # Update or create a new PI user
-        user_email = lines[22].split(',')[1].strip.downcase
+        user_email = lines[22].split(',',2)[-1].strip.downcase
         user = User.find_or_initialize_by_email(user_email)
         user.attributes =
           {
-            :name => lines[18].split(',')[1].strip,
-            :organization => lines[20].split(',')[1].strip
+            :name => lines[18].split(',',2)[-1].strip,
+            :organization => lines[20].split(',',2)[-1].strip
           }
 
         # Set a password for a new user
@@ -109,24 +109,24 @@ class Study < ActiveRecord::Base
           errors << "#{study.errors.full_messages.join(" and ")}"
         end
 
-        code = lines[2].split(',')[1].strip
+        code = lines[2].split(',',2)[-1].strip
         study = Study.find_or_initialize_by_code(code)
 
-        start_date = Date.parse(lines[8].split(',')[1])
-        end_date = Date.parse(lines[10].split(',')[1])
+        start_date = Date.parse(lines[8].split(',',2)[-1])
+        end_date = Date.parse(lines[10].split(',',2)[-1])
         study.attributes =
           {
-            :name => lines[6].split(',')[1].strip,
-            :title => lines[4].split(',')[1].strip,
+            :name => lines[6].split(',',2)[-1].strip,
+            :title => lines[4].split(',',2)[-1].strip,
             :start => start_date,
             :ending => end_date,
-            :description => lines[12].split(',')[1].strip,
-            :objectives => lines[14].split(',')[1].strip,
-            :investigators => lines[24].split(',')[1].strip,
-            :organizations => lines[26].split(',')[1].strip,
-            :funding => lines[28].split(',')[1].strip,
-            :benefits => lines[16].split(',')[1].strip,
-            :url => lines[30].split(',')[1].strip,
+            :description => lines[12].split(',',2)[-1].strip,
+            :objectives => lines[14].split(',',2)[-1].strip,
+            :investigators => lines[24].split(',',2)[-1].strip,
+            :organizations => lines[26].split(',',2)[-1].strip,
+            :funding => lines[28].split(',',2)[-1].strip,
+            :benefits => lines[16].split(',',2)[-1].strip,
+            :url => lines[30].split(',',2)[-1].strip,
             :user => user
           }
         unless study.valid?
