301d2f227a98eeed43b97c595d873a9b6eda8655
Kyle Wilcox <kwilcox@asascience.com>
2012-05-09 12:39:57 -0400

    CC admins on matched reports

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -15,7 +15,8 @@ class ReportMailer < ActionMailer::Base
   def matched_report_notification(report)
     @report = report
     recips = @report.tag_deployment.tag.study.user.email
-    mail(:to => recips, :subject => "[GLATOS] Tag Report Submitted")
+    ccs = User.where("role = 'admin'").map(&:email)
+    mail(:to => recips, :cc => ccs, :subject => "[GLATOS] Tag Report Submitted")
   end
 
 end
