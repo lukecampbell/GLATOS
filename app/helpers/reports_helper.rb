2b25aa001e4afe75c183d9894908a1200cc73ad5
Kyle Wilcox <kwilcox@asascience.com>
2012-09-18 16:03:37 -0400

    Fix change of nil object if a Tag has no active deployment

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -1,7 +1,7 @@
 module ReportsHelper
 
   def tags_and_active_deployments
-    select_tag "stuff", options_for_select(Tag.includes(:tag_deployments).map{|s| [s.code, s.active_deployment.id]}), :class => 'tagger', :include_blank => true
+    select_tag "stuff", options_for_select(Tag.includes(:tag_deployments).map{|s| [s.code, s.active_deployment.id ] if s.active_deployment }), :class => 'tagger', :include_blank => true
   end
 
 end
